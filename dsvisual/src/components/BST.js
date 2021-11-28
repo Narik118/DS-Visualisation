0b1accc40474800b5d29558a93b824cd708f2052
mohdshahbaaz08 <shahbaaz72870@gmail.com>
2021-11-28 14:53:07 +0530

    "some changes"

--- dsvisual/src/components/BST.js
+++ dsvisual/src/components/BST.js
@@ -3,20 +3,68 @@ import '../styles.css'
 import { Grid, Stack, Divider, Paper, TextField, Button } from '@mui/material'
 import { makeStyles, styled } from '@mui/styles';
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
-import { createMuiTheme, ThemeProvider } from '@mui/material';
+import { createMuiTheme, ThemeProvider,} from '@mui/material';
+import {BinarySearchTreeNode, VisualizationType, drawBinaryTree, BinaryTreeNode,} from 'binary-tree-visualizer';
+import { setTheme } from 'binary-tree-visualizer';
 
+ setTheme({
+     radius :25,
+     leafNodeSpace :90,
+     lineHeight: 120,
+     strokeColor:'#43eb34',
+     growthAndShrinkTimes: 1.25,
+     colorArray: [{
+         borderColor: "#43eb34",
+         bgColor:'#ffedfb',
+  }],
+
+
+ });
+
+//  class BST{
+//     constructor(value){
+//    if(this.value== value){
+//    return;
+//    }
+   
+//     if (value < this.value){
+//    if (this.left){
+//    this.left.insert(value);
+//    return;
+//    }
+//    if(this.right){
+//    this.right.insert(value);
+//    return;
+//    }
+//    this.setRight(new BSTNode(value));
+//    }
+//    }}
+ //Trees implimentation :)----------:(
+const root = new BinarySearchTreeNode(100); //Root node
+root.insert(50);
+root.insert(40);
+root.insert(110);
+root.insert(115);
+root.insert(109);
+root.insert(80);
+
+drawBinaryTree(root, document.querySelector('canvas'));
 
 const theme =  createMuiTheme({
      palette:{
          primary:{
-             main:'#800080'
-         }
+             main:'#f6f6f6'
+         } 
      },
+     overrides:{
+
+     }
 })
+
 
 const useStyles = makeStyles(theme=>({
     textFeild:{
-        borderColor:'orange'
+        color:'#f6f6f6'
     }
 }))
 
@@ -25,16 +73,17 @@ const BST = ()=>{
 
     //Material Compoent
     const Item = styled(Paper)(({ theme }) => ({
-        padding: theme.spacing(1.5),
+        padding: theme.spacing(1),
         textAlign: 'center',
-        width:'50px'
+        width:'50px',
+        marginLeft:'3px'
       }));
 
 
     //Validations states
     const [invalidInput, setInvalidInput] = useState(false)
     const [invalidBSTInput, setInvalidBSTInput] = useState(false)
-    const [helperMessage, setHelperMessage] = useState('Enter BST values [1,2,3,4]')
+    const [helperMessage, setHelperMessage] = useState('Insert Element (1):)')
 
 
     //state variables for actions
@@ -48,10 +97,6 @@ const BST = ()=>{
         <CreateBST/>
     },[BSTValues])
 
-    const BSTSizeInput = (e)=>{
-        setBSTSize(e.target.value)
-    }
-
     const onCreate=()=>{
         if(tempVal !== undefined &&tempVal.length !== undefined){
             console.log("entred here")
@@ -63,11 +108,6 @@ const BST = ()=>{
             }
                 if(Number(BSTSize)+1 === values.length){
                     setBSTValues(values)
-                }
-                else{
-                    alert("BST size is not equal to BST values")
-                    setInvalidInput(true)
-                    setInvalidBSTInput(true)
                 }
         }
         else if(tempVal === undefined){
@@ -91,23 +131,6 @@ const BST = ()=>{
     }
 
     const [newArr, setNewArr] = useState()
-    //pop function
-    const popFunction=()=>{
-        console.log(BSTValues.pop())
-
-        setNewArr(BSTValues.pop())
-        return(
-            <Stack justifyContent='center' direction="row" divider={<Divider orientation="vertical" flexItem />} spacing={1} >
-            <Stack direction='row'>
-            {newArr.map(item=>(
-                <Item id='itemId'>{item}</Item>
-            ))}
-            </Stack>
-        </Stack>
-        )
-    }
-
-
     const CreateBST=()=>{
         return(
         <Stack justifyContent='center' direction="row" divider={<Divider orientation="vertical" flexItem />} spacing={1} >
@@ -120,28 +143,55 @@ const BST = ()=>{
         )
     }
 
-    // return(
-    //     <div>
-    //         <ThemeProvider theme={theme}>
-    //     <Grid  direction='row' className='mainDiv' >
-    //         <Grid textAlign='center' className='inputClass'>
-    //             <TextField variant='outlined' className={classes.textFeild} id='inputId' label='Enter size' size='small' onChange={BSTSizeInput} type='number' error={invalidInput} helperText='Enter value less than 10'> </TextField> 
-    //             <TextField variant='outlined' id='inputId' label='Enter values' size='small' onChange={BSTInput} error={invalidBSTInput} helperText={helperMessage}> </TextField>
-    //             <Button variant='contained' id='createId' onClick={onCreate}>
-    //                 <NavigateNextIcon/>
-    //             </Button>
+    return(
+        <div>
+            <ThemeProvider theme={theme}>
+        <Grid  direction='row' className='mainDiv' >
+            <Grid textAlign='center' className='inputClass'>
+                <TextField variant='outlined' id='inputId' focused label='Insert' size='small' onChange={BSTInput} error={invalidBSTInput} helperText={helperMessage}> </TextField>
+                <Button variant='contained' id='createId' onClick={onCreate}>
+                
+                    <NavigateNextIcon/>
+                </Button>
 
-    //         </Grid>
-
-    //         <Grid> 
-    //             <CreateBST/>
-    //         </Grid>
-    //     </Grid>
-    //     </ThemeProvider>
-    //     <Button onClick={popFunction}>BST.pop()</Button>
-    //     </div>
-    // )
+            </Grid>
+        </Grid>
+        </ThemeProvider>
+        </div>
+        
+       )
+       
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const root = new BinarySearchTree(100);
+// [50,145,100,20,70,85,60,45,50].forEach(num) =root.insert(num);
+
+// drawBinaryTree(root, document.querySelector('canvas')),{
+//     Type:visualizationType.PRETTY,
+// }
 
 export default BST; 
 
